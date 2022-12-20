ce3221b220cf2dcca6810ec2051a1397d4b29ded
Swastik Dilip <justsyd224@gmail.com>
2022-12-20 12:39:55 +0000

    Testing draft pull request

--- api.py
+++ api.py
@@ -5,3 +5,7 @@ app=Flask(__name__)
 @app.route("/", methods=['GET'])
 def index():
     return 'Hello World'
+
+@app.route("/draftpull", methods=['GET'])
+def index():
+    return 'Draft Pull Request'
